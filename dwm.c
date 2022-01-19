94ed1a5e0e832b8a5bc7f54793683ad0feced096
rwxlio <lio.makki@gmail.com>
2022-01-19 19:32:10 +0100

    remove vacant indicator from tags

--- dwm.c
+++ dwm.c
@@ -927,7 +927,7 @@ drawbar(Monitor *m)
 	int x, w, tw = 0, stw = 0;
 	int boxs = drw->fonts->h / 9;
 	int boxw = drw->fonts->h / 6 + 2;
-	unsigned int i, occ = 0, urg = 0;
+	unsigned int i, urg = 0;
 	Client *c;
 
 	if(showsystray && m == systraytomon(m) && !systrayonleft)
@@ -943,7 +943,6 @@ drawbar(Monitor *m)
 
 	resizebarwin(m);
 	for (c = m->clients; c; c = c->next) {
-		occ |= c->tags;
 		if (c->isurgent)
 			urg |= c->tags;
 	}
@@ -952,10 +951,6 @@ drawbar(Monitor *m)
 		w = TEXTW(tags[i]);
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
-		if (occ & 1 << i)
-			drw_rect(drw, x + boxs, boxs, boxw, boxw,
-				m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
-				urg & 1 << i);
 		x += w;
 	}
 	w = blw = TEXTW(m->ltsymbol);
