5e42850374ea4dcaa3e7e6e114c26e97223c378c
rwxlio <lio.makki@gmail.com>
2022-02-07 09:23:15 +0100

    fix dmenu appearing position

--- config.def.h
+++ config.def.h
@@ -75,7 +75,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-h", "62",  NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-h", "62", "-m", dmenumon,  NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
